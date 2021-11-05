ae16aa870929e6b37c3258cc72c59fcbb3158c2f
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-05 23:09:16 +0000

    add user model

---
+++ models/user/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose')
+const Joi = require('joi')
+const bcrypt = require('bcryptjs')
+const emailRegexp = /^[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-]+@[a-zA-Z0-9]+[a-zA-Z0-9.-]+.[a-z]{2,4}$/
+
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      match: emailRegexp,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { versionKey: false, timestamps: true },
+)
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+}
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+const joiSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  balance: Joi.Number(),
+})
+
+const User = model('user', userSchema)
+module.exports = { User, joiSchema }
