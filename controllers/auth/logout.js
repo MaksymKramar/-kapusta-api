bc875e9f8326c1d4138e44893dee269ebef3362c
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-06 15:26:07 +0000

    added logout

---
+++ controllers/auth/logout.js
@@ -0,0 +1,20 @@
+const { User } = require('../../models')
+
+// eslint-disable-next-line no-unused-vars
+const logout = async (req, res, next) => {
+  try {
+    const { _id } = req.user
+    console.log(req.user)
+    await User.findByIdAndUpdate(_id, { token: null })
+
+    res.json({
+      status: 'success',
+      code: 204,
+      message: 'Logout',
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+module.exports = logout
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { auth: ctrl } = require('../../controllers')
 const { joiSchema } = require('../../models/user/user')
-const { validation } = require('../../middlewares')
+const { validation, authenticate } = require('../../middlewares')
 
 const router = express.Router()
 const userValidation = validation(joiSchema)
 
 router.post('/signup', userValidation, ctrl.signup)
 router.post('/login', userValidation, ctrl.login)
+router.get('/logout', authenticate, ctrl.logout)
 
 module.exports = router
