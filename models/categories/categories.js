b4340a0d98777a189a38d6b825ddcba5f2bd49c1
Dmytro Petrenko <blinker182ca@gmail.com>
2021-11-09 21:58:48 +0200

    edit category schema type

--- models/categories/categories.js
+++ models/categories/categories.js
@@ -1,33 +1,33 @@
-const { Schema, model, SchemaTypes } = require('mongoose')
-const Joi = require('joi')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
 const categorySchema = Schema(
   {
     title: {
       type: String,
-      required: [true, 'title is required'],
+      required: [true, "title is required"],
     },
     type: {
       type: String,
-      enum: ['expenses', 'incomes'],
-      required: [true, 'type is required'],
+      enum: ["expenses", "incomes"],
+      required: [true, "type is required"],
     },
     owner: {
-      type: SchemaTypes.ObjectId,
-      ref: 'user',
+      type: Schema.Types.ObjectId,
+      ref: "user",
     },
   },
-  { versionKey: false, timestamps: true },
-)
+  { versionKey: false, timestamps: true }
+);
 
 const joiSchema = Joi.object({
   title: Joi.string().min(2).max(15),
   type: Joi.string(),
-})
+});
 
-const Category = model('categories', categorySchema)
+const Category = model("categories", categorySchema);
 
 module.exports = {
   Category,
   joiSchema,
-}
+};
