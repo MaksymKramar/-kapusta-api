f9747b0db898be9476ea1b0f8cf4a5d710889bf8
Dmytro Petrenko <blinker182ca@gmail.com>
2021-11-09 22:01:37 +0200

    initial commit

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,12 +1,12 @@
-const { User } = require('../../models')
-const jwt = require('jsonwebtoken')
-const { NotFound } = require('http-errors')
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+const { NotFound } = require("http-errors");
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
     // console.log(user)
 
     if (!user || !user.comparePassword(password)) {
@@ -15,28 +15,29 @@ const login = async (req, res) => {
       //   code: 400,
       //   message: 'Email or password is wrong',
       // })
-      throw new NotFound('Email or password is wrong')
+      throw new NotFound("Email or password is wrong");
     }
-    const { _id } = user
+    const { _id } = user;
     const payload = {
       _id,
-    }
-    const { SECRET_KEY } = process.env
+    };
+    const { SECRET_KEY } = process.env;
 
-    const token = jwt.sign(payload, SECRET_KEY)
+    const token = jwt.sign(payload, SECRET_KEY);
 
-    await User.findByIdAndUpdate(user._id, { token })
+    await User.findByIdAndUpdate(user._id, { token });
 
     res.json({
-      status: '✔️ Success',
+      status: "✔️ Success",
       code: 200,
       data: {
         token,
       },
-    })
+    });
   } catch (error) {
-    res.status(400).json(error)
+    console.log(error.message);
+    res.status(400).json(error);
   }
-}
+};
 
-module.exports = login
+module.exports = login;
--- controllers/auth/singup.js
+++ controllers/auth/singup.js
@@ -1,28 +1,28 @@
-const { User } = require('../../models')
-const { Conflict } = require('http-errors')
+const { User } = require("../../models");
+const { Conflict } = require("http-errors");
 
 const signup = async (req, res) => {
   try {
-    const { name, email, password, balance } = req.body
-    console.log(req.body)
-    const user = await User.findOne({ email })
+    const { name, email, password, balance } = req.body;
+    // console.log(req.body)
+    const user = await User.findOne({ email });
 
     if (user) {
-      throw new Conflict('Already register')
+      throw new Conflict("Already register");
     }
 
-    const newUser = new User({ name, email, balance })
-    newUser.setPassword(password)
-    await newUser.save()
+    const newUser = new User({ name, email, balance });
+    newUser.setPassword(password);
+    await newUser.save();
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       code: 201,
       message: `✔️ Success register`,
       newUser,
-    })
+    });
   } catch (error) {
-    res.status(409).json(error)
+    res.status(409).json(error);
   }
-}
-module.exports = signup
+};
+module.exports = signup;
