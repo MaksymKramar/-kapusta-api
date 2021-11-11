9cefed063ec5b0f93faed38cfa31def77d9e194c
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-11 20:47:16 +0000

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,12 +1,12 @@
-const { User } = require("../../models");
-const jwt = require("jsonwebtoken");
-const { NotFound } = require("http-errors");
+const { User } = require('../../models')
+const jwt = require('jsonwebtoken')
+const { NotFound } = require('http-errors')
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
     // console.log(user)
 
     if (!user || !user.comparePassword(password)) {
@@ -15,29 +15,31 @@ const login = async (req, res) => {
       //   code: 400,
       //   message: 'Email or password is wrong',
       // })
-      throw new NotFound("Email or password is wrong");
+      throw new NotFound('Email or password is wrong')
     }
-    const { _id } = user;
+    const { _id } = user
     const payload = {
       _id,
-    };
-    const { SECRET_KEY } = process.env;
+    }
+    const { SECRET_KEY } = process.env
 
-    const token = jwt.sign(payload, SECRET_KEY);
+    const token = jwt.sign(payload, SECRET_KEY)
 
-    await User.findByIdAndUpdate(user._id, { token });
+    await User.findByIdAndUpdate(user._id, { token })
 
     res.json({
-      status: "✔️ Success",
+      status: '✔️ Success',
       code: 200,
       data: {
+        name: user.name,
+        email: user.email,
         token,
       },
-    });
+    })
   } catch (error) {
-    console.log(error.message);
-    res.status(400).json(error);
+    console.log(error.message)
+    res.status(400).json(error)
   }
-};
+}
 
-module.exports = login;
+module.exports = login
