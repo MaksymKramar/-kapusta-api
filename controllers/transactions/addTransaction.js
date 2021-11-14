9eba5e1bad4f8d68d4545b6dc2b1d308a66959af
Dmytro Petrenko <blinker182ca@gmail.com>
2021-11-14 23:29:52 +0200

    add date (day, month, year) fields for transaction

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -1,46 +1,43 @@
-const { Transaction, User } = require('../../models')
-const { sendSuccessRes } = require('../../utils')
+const { Transaction, User } = require("../../models");
+const { sendSuccessRes } = require("../../utils");
 
 const addTransaction = async (req, res) => {
-  //   console.log(req.body.type)
-  const { _id } = req.user
-  const { type } = req.body
-  const { sum } = req.body
+  const { _id } = req.user;
+  const { type, sum, date } = req.body;
 
-  console.log(type)
-
-  const newTransaction = { ...req.body, owner: _id }
-  const user = await User.findById(_id)
+  const [day, month, year] = date.split(".");
+  const newTransaction = { ...req.body, owner: _id, day, month, year };
+  const user = await User.findById(_id);
 
   try {
     if (!type) {
-      const newBalance = user.balance - sum
+      const newBalance = user.balance - sum;
 
       await User.findByIdAndUpdate(
         _id,
         { balance: newBalance },
         {
           new: true,
-        },
-      )
-      const result = await Transaction.create(newTransaction)
-      sendSuccessRes(res, { result }, 201)
+        }
+      );
+      const result = await Transaction.create(newTransaction);
+      sendSuccessRes(res, { result }, 201);
     } else {
-      const newBalance = user.balance + sum
+      const newBalance = user.balance + sum;
 
       await User.findByIdAndUpdate(
         _id,
         { balance: newBalance },
         {
           new: true,
-        },
-      )
-      const result = await Transaction.create(newTransaction)
-      sendSuccessRes(res, { result }, 201)
+        }
+      );
+      const result = await Transaction.create(newTransaction);
+      sendSuccessRes(res, { result }, 201);
     }
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(400).json(error.message);
   }
-}
+};
 
-module.exports = addTransaction
+module.exports = addTransaction;
