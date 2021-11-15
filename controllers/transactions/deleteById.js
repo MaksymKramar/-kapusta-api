64cd1ed9820dd430aba7bdb680d5b3f63fa8694b
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-15 20:01:24 +0000

    Update deleteById.js

--- controllers/transactions/deleteById.js
+++ controllers/transactions/deleteById.js
@@ -1,52 +1,56 @@
-const { Transaction, User } = require("../../models");
-const { sendSuccessRes } = require("../../utils");
-const { NotFound } = require("http-errors");
+const { Transaction, User } = require('../../models')
+const { sendSuccessRes } = require('../../utils')
+const { NotFound } = require('http-errors')
 
 // eslint-disable-next-line no-unused-vars
 const deleteById = async (req, res) => {
-  const { _id } = req.user;
-  const user = await User.findById(_id);
-  const { transactionId } = req.params;
+  const { _id } = req.user
+  const user = await User.findById(_id)
+  console.log(user)
+  const { transactionId } = req.params
 
-  const transaction = await Transaction.findById(transactionId);
-  const { type, sum } = transaction;
+  const transaction = await Transaction.findById(transactionId)
+  const { type } = transaction
+  const { sum } = transaction
+  console.log(type)
+  console.log(sum)
 
   try {
     if (!type) {
-      const newBalance = user.balance + sum;
+      const newBalance = user.balance + sum
 
       await User.findByIdAndUpdate(
         _id,
         { balance: newBalance },
         {
           new: true,
-        }
-      );
-      const result = await Transaction.findByIdAndDelete(transactionId);
+        },
+      )
+      const result = await Transaction.findByIdAndDelete(transactionId)
       if (!result) {
-        throw new NotFound(`Not found`); /* Проброс в error catch */
+        throw new NotFound(`Not found`) /* Проброс в error catch */
       }
-      sendSuccessRes(res, { message: "transaction deleted" });
+      sendSuccessRes(res, { message: 'transaction deleted' })
     } else {
-      const newBalance = user.balance - sum;
+      const newBalance = user.balance - sum
 
       await User.findByIdAndUpdate(
         _id,
         { balance: newBalance },
         {
           new: true,
-        }
-      );
-      const result = await Transaction.findByIdAndDelete(transactionId);
+        },
+      )
+      const result = await Transaction.findByIdAndDelete(transactionId)
       if (!result) {
-        throw new NotFound(`Not found`); /* Проброс в error catch */
+        throw new NotFound(`Not found`) /* Проброс в error catch */
       }
 
-      sendSuccessRes(res, { message: "transaction deleted" });
+      sendSuccessRes(res, { message: 'transaction deleted' })
     }
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).json(error.message)
   }
-};
+}
 
-module.exports = deleteById;
+module.exports = deleteById
