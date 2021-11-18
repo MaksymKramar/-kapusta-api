ec2a74c3015187c0c72b2f2713c6418181cfe5d2
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-18 20:34:41 +0000

    Update logout.js

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,25 +1,20 @@
-const { User } = require("../../models");
+const { User } = require('../../models')
 
 // eslint-disable-next-line no-unused-vars
 const logout = async (req, res, next) => {
   try {
-    const { _id } = req.user;
+    const { _id } = req.user
     // console.log(req.user)
-    await User.findByIdAndUpdate(_id, { token: null });
+    await User.findByIdAndUpdate(_id, { token: null })
 
-<<<<<<< Updated upstream
-    res.json({
-      status: "success",
-=======
     res.status(204).res.json({
       status: 'success',
->>>>>>> Stashed changes
       code: 204,
       message: `✔️ Logout`,
-    });
+    })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
-};
+}
 
-module.exports = logout;
+module.exports = logout
