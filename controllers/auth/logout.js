5dafc5e9077ce95a639a09d3c5bded3b9369fbfe
mykolasenyuk <73036879+mykolasenyuk@users.noreply.github.com>
2021-11-19 01:27:46 +0000

    Update logout.js

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -4,10 +4,10 @@ const { User } = require('../../models')
 const logout = async (req, res, next) => {
   try {
     const { _id } = req.user
-    // console.log(req.user)
+    console.log(req.user)
     await User.findByIdAndUpdate(_id, { token: null })
 
-    res.status(204).res.json({
+    res.status(204).json({
       status: 'success',
       code: 204,
       message: `✔️ Logout`,
