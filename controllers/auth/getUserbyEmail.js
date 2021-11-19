03b0778e8ca98c740722d0d133457059866a3fde
Dmytro Petrenko <blinker182ca@gmail.com>
2021-11-19 12:45:58 +0200

    add bad request error for controller

--- controllers/auth/getUserbyEmail.js
+++ controllers/auth/getUserbyEmail.js
@@ -1,24 +1,47 @@
-const { User } = require('../../models')
+const { User } = require("../../models");
 
 const getUserbyEmail = async (req, res) => {
-  try {
-    const { useremail } = req.params
-    console.log(useremail)
-    const currentUser = await User.findOne({ email: useremail })
-    console.log(currentUser)
+  //   try {
+  //     const { useremail } = req.params;
+  //     console.log(useremail);
+  //     const currentUser = await User.findOne({ email: useremail });
+  //     console.log(currentUser);
+  //     if (currentUser) {
+  //       res.status(200).json({
+  //         name: currentUser.name,
+  //         email: currentUser.email,
+  //         balance: currentUser.balance,
+  //         token: currentUser.token,
+  //       });
+  //     } else {
+  //       res.status(400).json({
+  //         staus: "error",
+  //         code: "400",
+  //         message: "Wrong email",
+  //       });
+  //     }
+  //   } catch (error) {
+  //     res.status(400).json(next(error));
+  //   }
+  // };
 
-    if (currentUser) {
-      res.status(200).json({
-        name: currentUser.name,
-        email: currentUser.email,
-        balance: currentUser.balance,
-        token: currentUser.token,
-      })
-      return
-    }
-  } catch (error) {
-    res.json(error)
+  const { useremail } = req.params;
+  const currentUser = await User.findOne({ email: useremail });
+
+  if (currentUser) {
+    res.status(200).json({
+      name: currentUser.name,
+      email: currentUser.email,
+      balance: currentUser.balance,
+      token: currentUser.token,
+    });
+  } else {
+    res.status(400).json({
+      staus: "error",
+      code: "400",
+      message: "Wrong email",
+    });
   }
-}
+};
 
-module.exports = getUserbyEmail
+module.exports = getUserbyEmail;
